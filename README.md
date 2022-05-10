e3700439dba10480ba4a6f1debc029266c10908f
lokesh17062001 <122003169@sastra.ac.in>
2022-05-10 11:54:21 +0530

    Updated with readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # psycopgorm
 django like orm built using psycopg postgres driver as base
+
+# Available Options
+
+1.Create a Table 
+
+2.Drop a Table
+
+3.Rename the Table Name
+
+4.Rename the Column Name in Table
+
+5.Change the Datatype of Column/Filed in Table
+
+0.Exit from DataBase
+
+# Usage
+
+*First select the appropiate option for the required action 
+
+*(Option-1):For Creating the table Give the Table Name , NUmber of Fields to be inserted and give the fiels and select the datatypes to assign
+
+*(Option-2):For Dropping the Table Give the table name 
+
+*(Option-3):For Rename The Table Name GIve the Table Name and NEw Table Name
+
+*(Option-4):For Rename the Column Name in some table GIve the Table Name,Column Name and New Column Name 
+
+*(Option-5):For Changing the datatype of field in table and give the table name , column/field name and give the new datatype
+
+*(Option-0):For Exit from database
--- pscyopgorm/dbplay.py
+++ pscyopgorm/dbplay.py
@@ -4,7 +4,13 @@ from psycopg2 import connect
 import psycopg2.extras
 
 class DBPlay:
+    """
+    Class for defining the definitions for creating dropping renaming tables,renaming table ,renaming datatypes and changing datatypes
+    """
     def __init__(self):
+        """
+        This is used for intialising the host,port,database by assigning user
+        """
         self.postgres_config = {
             "host":"localhost",
             "port":"5432",
@@ -13,7 +19,10 @@ class DBPlay:
             "password":"newpassword",
         }
         
-    def create_table(self,a:String,b:int):
+    def create_table(self,a:str,b:int) -> None:
+        """
+        Creating a table in database by passing the tablename and number of fields and entering field name and picking the datatype for field 
+        """
         li=[];
         c='('
         types=[' INT',' VARCHAR(25)']
@@ -44,7 +53,10 @@ class DBPlay:
         print('CREATE TABLE IF NOT EXISTS '+a+c)
         
         
-    def drop_table(self, a:String):
+    def drop_table(self, a:str):
+        """
+        Dropping the table from database by giving table name
+        """
         with connect(**self.postgres_config)as conn:
             conn.autocommit = True
             with conn.cursor() as curr:
@@ -56,7 +68,10 @@ class DBPlay:
                     print("Sorry!!! table doesn't exist")
                 curr.execute("DROP TABLE IF EXISTS "+a+";")
                 
-    def rename_table(self,a:String,b:String):
+    def rename_table(self,a:str,b:str):
+        """
+        Renaming the table in database by givig table name and new table name to be set 
+        """
         with connect(**self.postgres_config)as conn:
             conn.autocommit = True
             with conn.cursor() as curr:
@@ -67,7 +82,10 @@ class DBPlay:
                     print("Sorry!!! table doesn't exist")
                 curr.execute("ALTER TABLE IF EXISTS "+a+" RENAME TO "+b+";")
                 
-    def rename_field(self,a:String,b:String,c:String):
+    def rename_field(self,a:str,b:str,c:str):
+        """
+        Changing the field name in table by giving table name ,column/field name and new column/field name
+        """
         with connect(**self.postgres_config) as conn:
             conn.autocommit = True
             with conn.cursor() as curr:
@@ -81,7 +99,10 @@ class DBPlay:
                         print("Coulumn Renamed Successfully...")
                     else:
                         print("Sorry!!! Column doesn't exist")
-    def change_datatype(self,a:String,b:String,c:String):
+    def change_datatype(self,a:str,b:str,c:str):
+        """
+        Changing the field datatype in table by giving table name ,column/field name and new datatype
+        """
         with connect(**self.postgres_config) as conn:
             conn.autocommit=True
             with conn.cursor() as curr:
@@ -98,34 +119,35 @@ class DBPlay:
                         print("Coulumn Datatype Changed Successfully...")
                     else:
                         print("Sorry!!! Column doesn't exist")
-        
-                
+
+    def start_process():
+        map_dict = {
+            1: DBPlay.create_table,
+            2: DBPlay.drop_table,
+            3: DBPlay.rename_table,
+            4: DBPlay.rename_field,
+            5: DBPlay.change_datatype
+        }
+        print("Connected to db")
+        while(True):
+            print("Choose 1:create Table 2:Drop Table 3:Rename Table 4:Rename Column 5:Change the Column Datatype 0:Exit from database")
+            s = int(input("what u want! \n"))
+            if s == 0:
+                print("Disconnected from db")
+            elif s == 1:
+                map_dict[s](input("Table Name:"), int(
+                    input("Number of Fields:")))
+            elif s == 2:
+                map_dict[s](input("Table Name:"))
+            elif s == 3:
+                map_dict[s](input("Old Name:"), input("New Name:"))
+            elif s == 4:
+                map_dict[s](input("Table Name:"), input(
+                    "Old Name:"), input("New Name:"))
+            elif s == 5:
+                map_dict[s](input("Table Name:"), input(
+                    "Column Name:"), input("Datatype:").upper())
+
 
 if __name__ == "__main__":
-    db_obj = DBPlay()
-    map_dict = {
-        1:db_obj.create_table,
-        2:db_obj.drop_table,
-        3:db_obj.rename_table,
-        4:db_obj.rename_field,
-        5:db_obj.change_datatype
-    }
-    print("Connected to db")
-    while(True):
-        print("Choose 1:create Table 2:Drop Table 3:Rename Table 4:Rename Column 0:Exit from database")
-        s = int(input("what u want! \n"))
-        if s == 0:
-            print("Disconnected from db")
-        elif s == 1:
-            map_dict[s](input("Table Name:"),int(input("Number of Fields:")))
-        elif s == 2:
-            map_dict[s](input("Table Name:"))
-        elif s == 3:
-            map_dict[s](input("Old Name:"),input("New Name:"))
-        elif s == 4:
-            map_dict[s](input("Table Name:"),input("Old Name:"), input("New Name:"))
-        elif s == 5:
-            map_dict[s](input("Table Name:"),input("Column Name:"), input("Datatype:"))
-            
-        
-            
+    DBPlay.start_process()
